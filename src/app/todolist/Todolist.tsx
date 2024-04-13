5dd44a4efdb3a1051e3ddd8877d011975ba22e80
Nuttaphon <NuttaphonCP2023@gmail.com>
2024-04-13 22:44:29 +0700

    update todolist

--- src/app/todolist/Todolist.tsx
+++ src/app/todolist/Todolist.tsx
@@ -4,19 +4,23 @@ const Todolist = () => {
   const [text, setText] = useState([]);
   const [error, setError] = useState(false);
   const newText = () => {
-    const inText: string | null = (
-      document.getElementById("list") as HTMLInputElement
-    )?.value;
-    if (inText == "") {
+    const inText: string = (document.getElementById("list") as HTMLInputElement)
+      .value;
+    if (inText === "") {
       setError(true);
     } else {
       setText([inText, ...text]);
       (document.getElementById("list") as HTMLInputElement).value = "";
     }
   };
+
   const remove = (index: number) => {
     const clear = text.filter((item, indexCheck) => indexCheck != index);
     setText(clear);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
   };
 
   return (
@@ -29,28 +33,43 @@ const Todolist = () => {
         </nav>
         <div className="flex flex-col">
           <div className="flex gap-[0.5rem] flex-row">
-            <input
-              onChange={() => {
-                setError(false);
-              }}
-              className={`${
-                error ? "border-red-400 bg-red-300 font-bold text-red-700" : ""
-              } 
-              border-2 px-2 w-[70vw] py-2 rounded-[15px] bg-yellow-100 duration-[300ms] focus:bg-green-200`}
-              placeholder={`${
-                error ? "Please type a message." : "Type a message"
-              }`}
-              type="text"
-              name=""
-              id="list"
-              maxLength={60}
-            />
-            <button
-              onClick={newText}
-              className="btn1 border-2 px-4 py-2 rounded-[15px]
+            <form action="" onSubmit={handleSubmit}>
+              <input
+                onChange={() => {
+                  setError(false);
+                }}
+                className={`${
+                  error
+                    ? "border-red-400 bg-red-300 font-bold placeholder-gray-700"
+                    : "bg-yellow-100"
+                } 
+              border-2 px-2 w-[70vw] py-2 rounded-[15px]   duration-[300ms] focus:bg-green-200`}
+                placeholder={`${
+                  error ? "Please type a message." : "Type a message"
+                }`}
+                type="text"
+                name=""
+                id="list"
+                maxLength={60}
+              />
+              <button
+                type="submit"
+                onClick={newText}
+                className="btn1 border-2 px-4 py-2 rounded-[15px]
               bg-green-400 text-black font-[500] hover:bg-purple-400 duration-150"
+              >
+                Add
+              </button>
+            </form>
+            <button
+              type="submit"
+              onClick={() => {
+                setText([]);
+              }}
+              className="btn1 border-2 px-4 py-2 rounded-[15px]
+              bg-red-400 text-black font-[500] hover:bg-purple-400 duration-150"
             >
-              Add
+              Clear All Data
             </button>
           </div>
         </div>
@@ -87,9 +106,9 @@ const Todolist = () => {
                         onClick={() => {
                           remove(index);
                         }}
-                        className="bg-purple-800 text-white px-2 py-[2pxs] rounded-[8px]"
+                        className="bg-purple-800 text-white px-2 py-[2pxs] rounded-[8px] uppercase"
                       >
-                        Complete
+                        success
                       </button>
                     </div>
                   </div>
